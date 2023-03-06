58b5d4c0a4c3d862fa60c7756806e578515a6724
Andrew Ross <andrew.ross@takeda.com>
2023-03-06 01:21:18 -0600

    fresh

--- apps/express/api/logger.ts
+++ apps/express/api/logger.ts
@@ -6,13 +6,12 @@ export type RemoveFields<T, P extends keyof T = keyof T> = {
 
 const LoggerService = <
   T extends
-    | RemoveFields<winston.LoggerOptions, "level" | "transports">
+    | RemoveFields<winston.LoggerOptions, "format" | "transports">
     | undefined
 >(props?: {
   options?: T;
 }) =>
   winston.createLogger({
-    level: "debug",
     format: winston.format.prettyPrint({ colorize: true, depth: 4 }),
     transports: new winston.transports.Console(),
     ...props?.options
